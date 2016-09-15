418f2c8484e36f889711274c370a80602ea10a10
Jungsoo Park <jungsoopark96@gmail.com>
2016-09-15 16:14:27 -0400

    Add form submission using middleware
    Using body-parser

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/submit', function (req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+
+  console.log(name + " : " + email);
+  res.send(name + " : " + email);
 });
 
 app.listen(3000, function () {
